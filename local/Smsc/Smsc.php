b59794b812bb2e9f08843ba52f0f6fe2edf492b1
SERGEY <sm.developer@yandex.ru>
2023-12-11 16:40:32 +0300

    Доработка дизайна, добавление классов smsApi

---
+++ local/Smsc/Smsc.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once ($_SERVER['DOCUMENT_ROOT'] . '/local/Smsc/SmsApi.php');
+
+use Bitrix\Main\Error;
+use Bitrix\MessageService\Sender\Base;
+use Bitrix\MessageService\Sender\Result\SendMessage;
+
+\CModule::IncludeModule('messageservice');
+
+class Smsc extends \Bitrix\MessageService\Sender\Base
+{
+    private $login;
+
+    private $password;
+
+    private $client;
+
+    public function __construct() {
+        $this->login = '0882252';
+        $this->password = '5f0f175bcae410c672d55020b176efacf83aa965';
+
+        $this->client = new \SmsApi($this->login, $this->password);
+    }
+
+    public function sendMessage(array $messageFields) {
+        if (!$this->canUse()) {
+            $result = new SendMessage();
+            $result->addError(new Error('Ошибка отправки. СМС-сервис отключен'));
+            return $result;
+        }
+
+        $parameters = [
+            'phones' => $messageFields['MESSAGE_TO'],
+            'mes' => $messageFields['MESSAGE_BODY'],
+        ];
+
+        if ($messageFields['MESSAGE_FROM']) {
+            $parameters['sender'] = $messageFields['MESSAGE_FROM'];
+        }
+
+        $result = new SendMessage();
+        $response = $this->client->send($parameters);
+
+        if (!$response->isSuccess()) {
+            $result->addErrors($response->getErrors());
+            return $result;
+        }
+
+        return $result;
+    }
+
+    public function getShortName() {
+        return 'smsc.ru';
+    }
+
+    public function getId() {
+        return 'smscru';
+    }
+
+    public function getName() {
+        return 'SMS-центр';
+    }
+
+    public function canUse() {
+        return true;
+    }
+
+    public function getFromList():array {
+        $data = $this->client->getSenderList();
+        if ($data->isSuccess()) {
+            return $data->getData();
+        }
+
+        return [];
+    }
+}
