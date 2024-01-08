cefdee1b9280d0415fb4cb19178f7c78ca72457a
SERGEY <sm.developer@yandex.ru>
2024-01-08 08:37:53 +0300

    Доработки меню

--- include/menu/menu.top.php
+++ include/menu/menu.top.php
@@ -1,21 +1,24 @@
 <?global $arTheme;?>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"top",
-	Array(
-		"ALLOW_MULTI_SELECT" => "N",
+	"bitrix:menu", 
+	"top", 
+	array(
+		"ALLOW_MULTI_SELECT" => "Y",
 		"CHILD_MENU_TYPE" => "left",
 		"COMPONENT_TEMPLATE" => "top",
 		"COUNT_ITEM" => "6",
 		"DELAY" => "N",
-		"MAX_LEVEL" => $arTheme["MAX_DEPTH_MENU"]["VALUE"],
-		"MENU_CACHE_GET_VARS" => array(),
+		"MAX_LEVEL" => "3",
+		"MENU_CACHE_GET_VARS" => array(
+		),
 		"MENU_CACHE_TIME" => "3600000",
 		"MENU_CACHE_TYPE" => "A",
 		"MENU_CACHE_USE_GROUPS" => "N",
 		"CACHE_SELECTED_ITEMS" => "N",
-		"ALLOW_MULTI_SELECT" => "Y",
-		"ROOT_MENU_TYPE" => "top_content_multilevel",
-		"USE_EXT" => "Y"
-	)
+		"ROOT_MENU_TYPE" => "razdel_infoblock_uslug",
+		"USE_EXT" => "Y",
+		"COMPOSITE_FRAME_MODE" => "A",
+		"COMPOSITE_FRAME_TYPE" => "AUTO"
+	),
+	false
 );?>
